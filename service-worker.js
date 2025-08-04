c98b4e7ec86c0bf4b8af72b0d3c2a88af8830bb9
Michaeltse0426 <michaeltsegpt@gmail.com>
2025-08-04 13:40:14 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,1 @@
-const CACHE_NAME = "nordic-trip-cache-v2";
-const urlsToCache = ["index.html", "style.css", "script.js", "manifest.json", "itinerary.json"];
-
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+// Service worker placeholder
